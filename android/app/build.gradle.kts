3ba71be934313e3898ff9eb8225919836896ea03
3llips3s <kiurire.gichia@gmail.com>
2026-03-05 19:43:02 +0300

    chore: finalize project-page setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "dev.studio10200.hangmensch"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
